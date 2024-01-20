145a77454ff38f68c61c5a96841e6a298fd45e35
nma15 <namig.1000@gmail.com>
2024-01-20 12:54:05 +0300

    add funcs in channel.p

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,20 @@
+import os
+import json
+
+from googleapiclient.discovery import build
 
 
 class Channel:
     """Класс для ютуб-канала"""
 
+    api_key: str = os.getenv('YT-API-key')
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        response = self.youtube.channels().list(id=self.channel_id, part='snippet, statistics').execute()
+        print(json.dumps(response, indent=2, ensure_ascii=False))
